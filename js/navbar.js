f68519298951269210233808698dd2c325fe9d62
Konstantin Dudchenko <nav1gator87@yandex.ru>
2026-02-23 22:19:49 +0200

    Добавление фото на GitHub Pages

---
+++ js/navbar.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("navbar.html")
+    .then(response => response.text())
+    .then(html => {
+      document.getElementById("navbar").innerHTML = html;
+    });
+});
