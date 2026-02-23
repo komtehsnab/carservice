7e130bc82ea927d04aab37120c1b2d633c58119b
Konstantin Dudchenko <nav1gator87@yandex.ru>
2026-02-23 23:33:46 +0200

    Изменение путей

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,10 @@
-/*
+
 document.addEventListener("DOMContentLoaded", () => {
   fetch("navbar.html")
     .then(response => response.text())
     .then(html => {
       document.getElementById("navbar").innerHTML = html;
     });
-});*/
+});
+
+
